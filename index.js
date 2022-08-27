5adf31a22b48ce1555a7466ce7f60572bc022aa2
danielgathemia <danielgathemia01@gmail.com>
2022-08-27 09:36:40 +0300

    complete

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(people, action) {
+	let output = [];
+	let i = 0;
+	for (i = 0; i < people.length; i++) {
+		output.push(`Thank you, ${people[i]}, for the wonderful ${action} gift!`);
+	}
+	return output;
+}
+console.log(writeCards(["Guadalupe", "Ollie", "Aki"], "Surprise"));
+function countDown(number) {
+	while (number >= 0) {
+		console.log(number);
+		number--;
+	}
+}
